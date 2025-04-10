01e48b17668f158e38db9cbd8437f9e8fe6814b5
Mateo405913 <405913@tecnicatura.frc.utn.edu.ar>
2025-04-10 18:44:07 -0300

    ejercicio 2 hecho

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -13,11 +13,15 @@ public class App
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         // Complete this line
+        int b = scan.nextInt();
         // Complete this line
+        int c = scan.nextInt();
 
         System.out.println(a);
         // Complete this line
+        System.out.println(b);
         // Complete this line
+        System.out.println(c);
 
     }
 }
